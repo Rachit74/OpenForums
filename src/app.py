8d34cd9f494fdeeebd61e77d0c4ca000022534e6
Rachit6 <rachithooda09@gmail.com>
2021-07-19 08:31:57 +0530

    setup

---
+++ src/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
