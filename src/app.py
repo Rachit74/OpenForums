765857ebda74239014771ea9cb960a35904c7ba0
Rachit6 <rachithooda09@gmail.com>
2021-07-19 11:35:42 +0530

    home.html added and worked on index.html

--- src/app.py
+++ src/app.py
@@ -53,6 +53,11 @@ class RegisterForm(FlaskForm):
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+@login_required
+def home():
+    return render_template("home.html")
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -62,7 +67,7 @@ def login():
         if user:
             if check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
-                return redirect(url_for('index'))
+                return redirect(url_for('home'))
 
         return '<h1>Invalid username or password</h1>'
         #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
@@ -79,7 +84,7 @@ def signup():
         db.session.add(new_user)
         db.session.commit()
 
-        return '<h1>New user has been created!</h1>'
+        return redirect(url_for('home'))
         #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template('signup.html', form=form)
