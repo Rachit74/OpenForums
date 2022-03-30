bb2ffddd81a1520282d298d85f40b42b8ca7520f
Rachit74 <rachithooda09@gmail.com>
2022-03-30 08:06:23 +0530

    making like the classic forums site

--- studyforums/routes.py
+++ studyforums/routes.py
@@ -1,10 +1,5 @@
 from studyforums import *
 from studyforums.models import *
-from studyforums.forms import *
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
@@ -12,13 +7,11 @@ def index():
     return render_template('index.html', questions=questions)
 
 @app.route('/home')
-@login_required
 def home():
     questions = Question.query.all()
     return render_template("home.html", questions=questions)
 
 @app.route('/ask', methods=["POST", "GET"])
-@login_required
 def ask():
     if request.method == "POST":
         title = request.form['title']
@@ -52,48 +45,9 @@ def delete_ans(id):
     else:
         return "You cannot delete this post | <a href='/home'> Home </a>"
 
-@app.route('/question/<int:id>')
-@login_required
+@app.route('/post/<int:id>')
 def question(id):
     question = Question.query.get_or_404(id)
     answers = Answer.query.all()
     return render_template("question.html", q=question, user=current_user.username, answers=answers)
 
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('home'))
-
-        return render_template('login.html', err="Invalid credentials! Please try again.", form=form)
-        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
-
-    return render_template('login.html', form=form)
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect(url_for('home'))
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
-
-    return render_template('signup.html', form=form)
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
